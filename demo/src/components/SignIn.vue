f4693ea5e45f062f922485d78a3e2e8ebe1d7eb9
wufei0429 <2453804076@qq.com>
2019-03-21 20:45:27 +0800

    vue study demo

---
+++ demo/src/components/SignIn.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container">
+		<h2>登录</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Message',
+		data(){
+			return {
+				msg:'这是登录页面'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+</style>
